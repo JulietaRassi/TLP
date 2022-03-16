ca21981d54f717da3b53309cb6813fe23456b6c1
JulietaRassi <101115170+JulietaRassi@users.noreply.github.com>
2022-03-16 14:50:09 -0300

    Add files via upload

--- rassi_martinetto.sql
+++ rassi_martinetto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-03-2022 a las 14:14:10
+-- Tiempo de generación: 16-03-2022 a las 18:48:42
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -35,6 +35,17 @@ CREATE TABLE `alumnos` (
   `teléfono` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id_legajo`, `nombre`, `apellido`, `domicilio`, `teléfono`) VALUES
+(1, 'Pedro', 'Sanchez', 'Nuevacordoba3634', 1523064785),
+(2, 'Bruno', 'Salas', 'Chacabuco305', 152214702),
+(3, 'Camila', 'Linaza', 'Bulevarilias43', 152305469),
+(4, 'Lara', 'Muzzio', 'Obispotrejo382', 1529118784),
+(5, 'Gilda', 'Trento', 'Independencia451', 1524036699);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +62,17 @@ CREATE TABLE `empresa` (
   `edad` int(11) DEFAULT NULL,
   `fecha de nacimiento` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`id_cliente`, `nombre`, `apellido`, `DNI`, `telefono`, `domicilio`, `edad`, `fecha de nacimiento`) VALUES
+(1, 'Mariana', 'Lopez', 29347122, 4850078, 'StaRosa560', 28, '1997-04-14'),
+(2, 'Azul', 'Martinetto', 46450429, 1522303238, 'Suecia2918', 16, '2005-06-01'),
+(3, 'Julieta', 'Rassi', 45629627, 1526504868, 'Ituzaingo115', 17, '2004-08-23'),
+(4, 'Ana', 'Torres', 54874592, 351024871, 'AvBuenosAires123', 30, '1992-02-15'),
+(5, 'Camilo', 'Romero', 41720392, 15247483, 'Altacordoba481', 20, '2002-11-25');
 
 --
 -- Índices para tablas volcadas
@@ -76,13 +98,13 @@ ALTER TABLE `empresa`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id_legajo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_legajo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
